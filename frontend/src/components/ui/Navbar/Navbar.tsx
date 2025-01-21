eea678d5960c67e38b55251250e73cdd1a1b1a8d
Elijah Khor <elijahkcm@gmail.com>
2025-01-21 13:52:14 -0800

    minor adjustments to FE

--- frontend/src/components/ui/Navbar/Navbar.tsx
+++ frontend/src/components/ui/Navbar/Navbar.tsx
@@ -3,14 +3,17 @@ import { Separator } from "@/components/ui/separator"
 export const Navbar = () => {
     return (
       <nav>
-        <div>
+        <div className="w-3/4 mx-auto">
           <div className="flex justify-center items-center h-16">
             <h1 className="text-2xl font-bold">
                 PlayStation x MIT Reddit Sentiment Analysis
             </h1>
           </div>
-          <p className="text-center">
-            This is a small project as part of the MIT course. Note: insights generated are only for the /r/thelastofus subreddit
+          <p className="text-center mx-auto">
+            This is a small project for the IAP - Sony Interactive Entertainment Comes to MIT: The Nexus of Games and AI Course.
+            This project is a proof of concept to show how large language models can be used for sentiment analysis on social media.
+            
+            Note: data is scraped from the <a href="https://www.reddit.com/r/thelastofus/" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800 transition-colors">/r/thelastofus</a> subreddit.
           </p>
         </div>
         <Separator className="my-4" />
